1e4cc851deb2bfd69c76952539a4827b1ce9c497
Yuriy Lvov <y.lvov.ca@gmail.com>
2023-05-09 20:32:09 -0400

    split messages

--- frontend/src/components/chat-messages/index.jsx
+++ frontend/src/components/chat-messages/index.jsx
@@ -1,20 +1,38 @@
+import { useContext } from 'react';
 import { useSelector } from 'react-redux';
-import { Col } from 'react-bootstrap';
+import { Alert, Col } from 'react-bootstrap';
 import filter from 'leo-profanity';
+import cn from 'classnames';
 import { selectMessages } from '../../features/chats';
+import { AuthContext } from '../../context/auth';
 
 const ChatMessages = () => {
+  const { username } = useContext(AuthContext);
   const messages = useSelector(selectMessages);
 
   return (
     <Col className="chat-messages overflow-auto px-5 d-flex flex-column justify-content-end">
-      {messages.map((message) => (
-        <div key={message.id}>
-          <b>{message.username}</b>
-          <span>: </span>
-          <span>{filter.clean(message.body)}</span>
-        </div>
-      ))}
+      {messages.map((message) => {
+        const isUserMessage = message.username === username;
+
+        return (
+          <div
+            className={cn('d-flex', {
+              'justify-content-end': isUserMessage,
+            })}
+            key={message.id}
+          >
+            <Alert
+              className="w-75"
+              variant={isUserMessage ? 'primary' : 'warning'}
+            >
+              <b>{message.username}</b>
+              <span>: </span>
+              <span>{filter.clean(message.body)}</span>
+            </Alert>
+          </div>
+        );
+      })}
     </Col>
   );
 };
